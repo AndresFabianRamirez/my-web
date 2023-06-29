233ada0e4b2d62fd50615c9990812786d91b6c8f
andres <andrecitoramirez11@gmail.com>
2023-06-29 10:13:26 -0500

    mejorado

--- progjs.js
+++ progjs.js
@@ -1,1 +1,19 @@
-console.log("hola mundo");
+const h1 = document.querySelector('h1');
+//selecciono desde mi archivo en Html el o los apartados con la etiqueta h1
+const p = document.querySelector('p');
+// selecciono los apartados con la etiqueta p
+const parrafito = document.querySelector('.parrafito');
+// .parrafito se refiere a la clase parrafito, escoge los apartados asignados a esa clase
+const pid = document.querySelector('#pid')
+//·pid se refiere al ID: pid, me trae los apartados con ese ID
+const input =document.querySelector("input");
+
+console.log(h1);
+console.log({
+    h1,
+    p,
+parrafito,
+pid,
+input,
+});
+
